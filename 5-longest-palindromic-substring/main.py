03acfedb712753744dd6e40ee2bf6ee3c7cfed60
Hang Hoo <442869088@qq.com>
2024-08-02 23:06:49 -0400

    Create main.py

---
+++ 5-longest-palindromic-substring/main.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        res = ""
+        for i in range(len(s)):
+            s1 = self.palindrome(s, i, i)
+            s2 = self.palindrome(s, i, i + 1)
+            res = res if len(res) > len(s1) else s1
+            res = res if len(res) > len(s2) else s2
+        return res
+
+    def palindrome(self, s: str, l: int, r: int) -> str:
+        # 防止索引越界
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            # 向两边展开
+            l -= 1
+            r += 1
+        # 返回以 s[l] 和 s[r] 为中心的最长回文串
+        return s[l+1:r]
